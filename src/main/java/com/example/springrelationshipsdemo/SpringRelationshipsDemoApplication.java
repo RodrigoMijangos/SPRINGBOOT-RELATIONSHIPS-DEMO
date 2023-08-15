dbf34ae922abab0068e1e2108a4bbce1843ec10f
Rodrigo Mijangos <rdrgmijangos@gmail.com>
2023-08-15 13:29:43 -0600

    Initial state of the application

---
+++ src/main/java/com/example/springrelationshipsdemo/SpringRelationshipsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springrelationshipsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRelationshipsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRelationshipsDemoApplication.class, args);
+    }
+
+}
